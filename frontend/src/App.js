cc2a3f0fea0825953360ad09cb6efb7fd5abed67
mukund jajadiya <mukundjajadiya2017@gmail.com>
2022-01-18 12:35:32 +0530

    V 1.0.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,9 +32,9 @@ const App = () => {
 			"User-Agent":
 				"Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
 		};
-		formData.append("file", d, { headers = headers });
+		formData.append("file", d);
 
-		axios.post(URL, formData).then((res) => {
+		axios.post(URL, formData, headers=headers).then((res) => {
 			console.log(res.data);
 			setPrediction(res.data["prediction"]);
 			setSend(true);
